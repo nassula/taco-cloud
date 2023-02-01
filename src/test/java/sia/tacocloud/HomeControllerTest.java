6a8c57a2533460188f7260165b599d5daed748d8
Diego Nassula <diegonassula@gmail.com>
2023-02-01 19:18:06 -0300

    Initial commit

---
+++ src/test/java/sia/tacocloud/HomeControllerTest.java
@@ -0,0 +1,23 @@
+package sia.tacocloud;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(HomeController.class)
+class HomeControllerTest {
+
+    private MockMvc mockMvc;
+
+    public void testHomePage() throws Exception {
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(content().string(containsString("Welcome to...")));
+    }
+
+}
