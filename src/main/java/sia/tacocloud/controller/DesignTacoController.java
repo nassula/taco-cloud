aefac65a22c73adbc64b622f3c0d5288429b1b02
Diego Nassula <diegonassula@gmail.com>
2023-02-04 11:06:06 -0300

    With this commit I finish the second chapter of the book.

--- src/main/java/sia/tacocloud/controller/DesignTacoController.java
+++ src/main/java/sia/tacocloud/controller/DesignTacoController.java
@@ -1,12 +1,11 @@
 package sia.tacocloud.controller;
 
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
 import sia.tacocloud.domain.constants.Type;
 import sia.tacocloud.domain.entities.Ingredient;
 import sia.tacocloud.domain.entities.Taco;
@@ -56,7 +55,20 @@ public class DesignTacoController {
 
     @GetMapping
     public String showDesignForm(){
-        return "design";
+        return "design-a-taco";
+    }
+
+    @PostMapping
+    public String processTaco(@Valid Taco taco, Errors errors, @ModelAttribute TacoOrder tacoOrder){
+
+        if (errors.hasErrors()){
+            return "design-a-taco";
+        }
+
+        tacoOrder.addTaco(taco);
+        log.info("Processing taco: {}", taco);
+
+        return "redirect:/orders/current";
     }
 
     private Iterable<Ingredient> filterByType(List<Ingredient> ingredients, Type type){
